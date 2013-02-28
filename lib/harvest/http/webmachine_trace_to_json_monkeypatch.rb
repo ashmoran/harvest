7db523641b3d55a64d045c8ebc9d4b5c92d365e4
Ash Moran <ash.moran@patchspace.co.uk>
2013-02-28 10:45:24 +0000

    Remove unhelpful warning

--- lib/harvest/http/webmachine_trace_to_json_monkeypatch.rb
+++ lib/harvest/http/webmachine_trace_to_json_monkeypatch.rb
@@ -2,10 +2,6 @@
 module Webmachine
   module Trace
     class ResourceProxy
-      if ResourceProxy.instance_methods.include?(:to_json)
-        puts "WARNING! Overriding ResourceProxy#to_json"
-      end
-
       def to_json
         @resource.to_json
       end
