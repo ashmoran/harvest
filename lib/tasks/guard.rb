7b94b3641d5d03e7236fc03a30d5fba33f39707a
Ash Moran <ash.moran@patchspace.co.uk>
2013-03-08 16:59:08 +0000

    Amend: Default rake task to quickly start guard with the direct domain client

--- lib/tasks/guard.rb
+++ lib/tasks/guard.rb
@@ -6,6 +6,11 @@ namespace :guard do
     system "guard"
   end
 
+  desc "Run guard in WIP mode with the default adapter"
+  task :wip do
+    system "GUARD_MODE=wip guard"
+  end
+
   desc "Run guard with the HTTP adapter"
   task :http do
     system "HARVEST_INTERFACE=http guard"
