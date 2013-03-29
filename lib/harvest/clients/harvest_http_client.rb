72771c4c0cb430b17b9273efcc307279cda1a11e
Ash Moran <ash.moran@patchspace.co.uk>
2013-03-29 15:56:02 +0000

    Stateful HTTP client: Kill legacy non-hypermedia client code

--- lib/harvest/clients/harvest_http_client.rb
+++ lib/harvest/clients/harvest_http_client.rb
@@ -185,83 +185,6 @@ module Harvest
         }
       end
 
-      # Legacy implementation
-
-      def poseidon
-        self
-      end
-
-      def read_models
-        self
-      end
-
-      def [](read_model_name)
-        case read_model_name
-        when :registered_fishermen
-          registrar_link = @api.get.body.links[:"fisherman-registrar"].href
-          record_array(@api.get(registrar_link).body.resources[:"registered-fishermen"].map(&:symbolize_keys))
-        when :fishing_grounds_available_to_join
-          fishing_world_link = @api.get.body.links[:"fishing-world"].href
-          view_model(
-            EventHandlers::ReadModels::FishingGroundsAvailableToJoin,
-            @api.get(fishing_world_link).
-              body.
-              resources[:"fishing-grounds-available-to-join"].
-              map(&:symbolize_keys).
-              map { |ground| ground.merge(uuid: UUIDTools::UUID.parse(ground[:uuid])) }
-          )
-        when :fishing_ground_businesses
-          fishing_world_link = @api.get.body.links[:"fishing-world"].href
-          fishing_grounds = @api.get(fishing_world_link).body.resources[:"fishing-grounds-available-to-join"]
-
-          # The domain model code gets a read model back that contains all businesses for
-          # all fishing grounds. The hypermedia UI currently segregates them into embedded
-          # resources, so we join them back together to fake the read model view. Nasty,
-          # but it gets us going.
-          businesses = fishing_grounds.map { |fishing_ground|
-            fishing_ground["_embedded"]["fishing-ground-businesses"]
-          }.flatten
-
-          view_model(
-            EventHandlers::ReadModels::FishingGroundBusinesses,
-            # WTF
-            businesses.map(&:symbolize_keys).map { |business|
-              business.merge(
-                uuid: UUIDTools::UUID.parse(business[:uuid]),
-                fishing_business_uuid: UUIDTools::UUID.parse(business[:fishing_business_uuid])
-              )
-            }
-          )
-        when :fishing_business_statistics
-          fishing_world_link = @api.get.body.links[:"fishing-world"].href
-          fishing_grounds = @api.get(fishing_world_link).body.resources[:"fishing-grounds-available-to-join"]
-
-          # Same problem as :fishing_ground_businesses - we make a request per fishing ground!
-          businesses = fishing_grounds.map { |fishing_ground|
-            statistics_link = fishing_ground["_links"]["statistics"]["href"]
-            @api.get(statistics_link).body.resources["fishing-business-statistics"]
-          }.flatten
-
-          # Holy shit
-          records =
-            businesses.map(&:symbolize_keys).map { |business|
-              {
-                fishing_ground_uuid:    UUIDTools::UUID.parse(business[:fishing_ground_uuid]),
-                fishing_business_uuid:  UUIDTools::UUID.parse(business[:fishing_business_uuid]),
-                lifetime_fish_caught:   business[:lifetime_fish_caught].to_i,
-                lifetime_profit:        Harvest::Domain::Currency.dollar(business[:lifetime_profit].sub(/^\$/, "").to_i)
-              }
-            }
-
-          view_model(
-            EventHandlers::ReadModels::FishingBusinessStatistics,
-            records
-          )
-        else
-          raise "Unimplemented HTTP read model!"
-        end
-      end
-
       private
 
       def move_to_root_resource
@@ -271,23 +194,6 @@ module Harvest
       def move_to_resource(uri)
         @current_resource = @api.get(uri)
         raise "Error loading resource #{@current_resource.inspect}" unless @current_resource.success?
-      end
-
-      # Fake the interface the Cucumber steps currently require
-      def record_array(array)
-        def array.records
-          self
-        end
-        array
-      end
-
-      # Fake the interface the Cucumber steps currently require
-      def view_model(model_class, records)
-        database = InMemoryReadModelDatabase.new
-        records.each do |record|
-          database.save(record)
-        end
-        model_class.new(database)
       end
     end
   end
