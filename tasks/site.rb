4fc965b0d531848d8847d7fac35b3f6d7c842db8
Ash Moran <ash.moran@patchspace.co.uk>
2013-07-08 12:33:41 +0100

    Amend: Basic build system for vendor JS into web_client/site/

--- tasks/site.rb
+++ tasks/site.rb
@@ -9,5 +9,10 @@ namespace :site do
   task :clean do
     FileUtils.rm_rf("web_client/site")
     FileUtils.mkdir_p("web_client/site")
+
+    FileUtils.mkdir_p("web_client/site/lib")
+    FileUtils.mkdir_p("web_client/site/fonts")
+    FileUtils.mkdir_p("web_client/site/images")
+    FileUtils.mkdir_p("web_client/site/styles")
   end
 end
