c4c58e9b9d98f65b56dbb5a85d1ba5f4fe441147
Ash Moran <ash.moran@patchspace.co.uk>
2013-07-15 14:32:44 +0100

    Webmachine TestResponse to make inspecting errors a bit easier

---
+++ app_server/spec/support/test_response.rb
@@ -0,0 +1,70 @@
+require 'forwardable'
+require 'term/ansicolor'
+require 'ap'
+
+RSpec::Matchers.define :have_status_code do |expected_code|
+  match do |response|
+    response.code == expected_code
+  end
+
+  # TODO: only want to log the error on unexpected 500s etc
+  failure_message_for_should do |response|
+    "Expected response #{response} to have status #{expected_code}\n" +
+    "Got: #{response.code}\n" +
+    (response.error || "")
+  end
+end
+
+module Webmachine
+  # A decorator for resources to help in testing
+  class TestResponse
+    include Term::ANSIColor
+    extend Forwardable
+
+    def_delegators :@response,
+      :headers, :code, :code=, :body, :body=, :redirect, :trace, :error, :error=,
+      :do_redirect, :set_cookie,
+      :is_redirect?, :redirect_to
+
+    class << self
+      def build
+        new(Webmachine::Response.new)
+      end
+    end
+
+    def initialize(response)
+      @response = response
+      @inspector = AwesomePrint::Inspector.new(
+        multiline: false, color: { symbol: :purpleish }
+      )
+    end
+
+    def trace_lines
+      trace.map { |trace_hash|
+        format_trace_line(trace_hash)
+      }
+    end
+
+    # Handling all cases the same here because I'm no longer sure
+    # if we want to do error formatting in this method
+    def format_trace_line(line)
+      case line[:type]
+      when :response
+        case line[:code].to_i
+        when 500
+          format_standard_line(line)
+        else
+          format_standard_line(line)
+        end
+      else
+        format_standard_line(line)
+      end
+    end
+
+    def format_standard_line(line)
+      line.inject([ ]) { |line_parts, (key, value)|
+        line_parts.concat([ "#{cyan(key.to_s)}: #{@inspector.awesome(value)}" ])
+      }.join(", ")
+    end
+  end
+end
