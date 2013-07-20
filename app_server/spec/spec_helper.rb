8c7e66d1a92dcdefcde3265b8e597cf7c8024054
Ash Moran <ash.moran@patchspace.co.uk>
2013-07-20 15:07:26 +0100

    Allow filtering RSpec examples by `focus: true`

--- app_server/spec/spec_helper.rb
+++ app_server/spec/spec_helper.rb
@@ -11,8 +11,18 @@ require 'ap'
 require 'webmock/rspec'
 
 RSpec.configure do |config|
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
+
   config.expect_with :rspec do |c|
     # Disable the `should` syntax
     c.syntax = :expect
+  end
+
+  config.before(:each, allow_net_connect: true) do
+    WebMock.allow_net_connect!
+  end
+  config.after(:each, allow_net_connect: true) do
+    WebMock.disable_net_connect!
   end
 end
