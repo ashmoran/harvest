5b417820bbba90e9a90dfd3cbb5a226a65fbb4e4
Ash Moran <ash.moran@patchspace.co.uk>
2013-07-20 15:49:00 +0100

    Remove the FishingApplication representation (now a command)

--- app_server/lib/harvest/http/representations.rb
+++ app_server/lib/harvest/http/representations.rb
@@ -7,7 +7,6 @@ module Harvest; module HTTP; module Representations; end; end; end
 
 require_relative 'representations/fisherman'
 require_relative 'representations/fisherman_registrar'
-require_relative 'representations/fishing_application'
 require_relative 'representations/fishing_business_application'
 require_relative 'representations/fishing_business_statistics'
 require_relative 'representations/fishing_ground'
