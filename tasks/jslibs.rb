3bfed181b44895a12982be3b6265e8b24fade93c
Ash Moran <ash.moran@patchspace.co.uk>
2013-07-30 12:21:05 +0100

    Build pipeline to compile and minify CoffeeScript with Source Maps

--- tasks/jslibs.rb
+++ tasks/jslibs.rb
@@ -9,22 +9,59 @@ namespace :jslibs do
 
   namespace :build do
     def coffee(args)
-      system "cd web_client/www/lib/harvest; #{PROJECT_DIR}/node_modules/coffee-script/bin/coffee #{args}"
+      system "cd web_client/www/lib/harvest; " +
+             "#{PROJECT_DIR}/node_modules/coffee-script/bin/coffee #{args}"
+    end
+
+    def mapcat(args)
+      system "cd web_client/www/lib/harvest; " +
+             "#{PROJECT_DIR}/node_modules/mapcat/bin/mapcat #{args}"
+    end
+
+    def uglifyjs(args)
+      system "cd web_client/www/lib/harvest; " +
+             "#{PROJECT_DIR}/node_modules/uglify-js/bin/uglifyjs #{args}"
+    end
+
+    def relative(prerequisites)
+      prerequisites.map { |prerequisite|
+        prerequisite.sub("web_client/www/lib/harvest/", "")
+      }
+    end
+
+    # Chrome 28 only understands //@ syntax, Chrome 29 will understand //#
+    def revert_to_legacy_sourcemap_syntax(file)
+      system "sed -E -i '' '/^\\/\\/#/s/^\\/\\/#/\\/\\/@/' #{file}"
     end
 
     desc "Build all JavaScript files"
     task :all => [
-      'web_client/www/lib/harvest',
-      'web_client/www/lib/harvest/signup_form.js'
+      "web_client/www/lib/harvest",
+      "web_client/www/lib/harvest/signup.min.js"
     ]
 
-    file 'web_client/www/lib/harvest/signup_form.js' => [
-      'web_client/www/lib/harvest/signup_form.coffee',
+    file "web_client/www/lib/harvest/signup.min.js" => [
+      "web_client/www/lib/harvest/signup.js"
     ] do |t|
-      relative_prerequisites = t.prerequisites.map { |prereq|
-        prereq.sub("web_client/www/lib/harvest/", "")
-      }
-      coffee "--compile --map #{relative_prerequisites.join(" ")}"
+      uglifyjs "--compress --mangle --source-map signup.min.map --in-source-map signup.map -o signup.min.js signup.js"
+      revert_to_legacy_sourcemap_syntax(t.name)
+    end
+
+    file "web_client/www/lib/harvest/signup.js" => [
+      "web_client/www/lib/harvest/signup_form.js",
+      "web_client/www/lib/harvest/calm_delegate.js",
+      "web_client/www/lib/harvest/signup_service.js"
+    ] do |t|
+      mapcat "-j signup.js -m signup.map #{relative(t.prerequisites).map { |js| js.sub(".js", ".map") }.join(" ")}"
+      revert_to_legacy_sourcemap_syntax(t.name)
+    end
+
+    %w[ signup_form signup_service calm_delegate ].each do |coffescript_file|
+      file "web_client/www/lib/harvest/#{coffescript_file}.js" => [
+        "web_client/www/lib/harvest/#{coffescript_file}.coffee",
+      ] do |t|
+        coffee "--compile --map #{relative(t.prerequisites).join(" ")}"
+      end
     end
 
     source_coffee_filename =
@@ -36,6 +73,6 @@ namespace :jslibs do
       FileUtils.cp(source_coffee_filename[t.name], t.name)
     end
 
-    directory 'web_client/www/lib/harvest'
+    directory "web_client/www/lib/harvest"
   end
 end
